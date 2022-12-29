d6a12434be35214a436e655ab6c0e42c071a1e8b
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-30 01:14:04 +0500

    making registration form

---
+++ reg_form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="regstyle.css">
+</head>
+
+<body>
+<span>sdghfjkmv</span>
+</body>
+</html>
