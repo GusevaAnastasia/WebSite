7bd26b2ee5885eeeb3e3b64291b11df0ebce30de
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-30 01:55:44 +0500

    styles changes

--- reg_form.php
+++ reg_form.php
@@ -3,10 +3,33 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="regstyle.css">
+  <link rel = "stylesheet" href = "css/regstyle.css" type="text/css" charset="utf-8">
+
 </head>
 
 <body>
-<span>sdghfjkmv</span>
-</body>
-</html>
+ <h2 class="heading">Регистрация</h2>
+ <div style="clear:both"><br></div>
+ <div class="">
+ <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2">
+  </div>
+
+  <button type="submit" class="btn">Submit</button>
+ </form>
+ </div>
+ </body>
+ </html>
