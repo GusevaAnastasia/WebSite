688ed54037e6ad6f63a23b23ac61d86277cea23f
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-30 10:07:24 +0500

    exit file, message about succsexxful enter

---
+++ exit.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<link rel = "stylesheet" href = "css/regstyle.css" type="text/css" charset="utf-8">
+    <link rel = "stylesheet" href = "css/main_page.css" type="text/css" charset="utf-8">
+    <link rel = "stylesheet" href = "css/main.css" type="text/css" charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div class="heading">
+	<h1>Привет!</h1>
+	<a href="exit.php">Что бы выйти нажмите по ссылке</a></div>
+
+</body>
+</html>
