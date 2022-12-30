b5965e8b7c3e5a336337bb92b78b1098b713a51d
GusevaAnastasia <aguseva.03@mail.ru>
2022-12-30 09:44:46 +0500

    adding a password length check

--- check.php
+++ check.php
@@ -2,4 +2,14 @@
 
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING); 
 $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
-$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING); ?>
+$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING); 
+
+if(mb_strlen($login) < 5 || mb_strlen($login) > 90){
+	echo "Недопустимая длина логина";
+	exit();
+}
+else if(mb_strlen($name) < 5){
+	echo "Недопустимая длина имени.";
+	exit();
+} 
+?>
